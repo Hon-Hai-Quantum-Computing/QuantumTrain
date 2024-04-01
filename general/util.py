0f3b1daf104c8d10beee1c932148d648ad6b6f4b
qoohehe <wei.jia.huang.physics@gmail.com>
2024-04-01 09:35:27 +0000

    change LewHybridNN structure

--- general/util.py
+++ general/util.py
@@ -125,121 +125,248 @@ def network_config_extract(model):
     
     return network_config
 
-def QuantumTrain(model, n_qubit, nw_list_normal, q_depth, device, network_config):
-    class LewHybridNN(nn.Module):
-        class QLayer(nn.Module):
-            def __init__(self, n_blocks):
-                super().__init__()
-                self.n_wires = int(np.ceil(np.log2(len(nw_list_normal)))),
-                self.n_wires = self.n_wires[0]
-                self.n_blocks = n_blocks
-                self.u3_layers = tq.QuantumModuleList()
-                self.cu3_layers = tq.QuantumModuleList()
-                # self.measure = tq.MeasureAll(tq.PauliZ)
-                for _ in range(self.n_blocks):
-                    self.u3_layers.append(
-                        tq.Op1QAllLayer(
-                            op=tq.U3,
-                            n_wires=self.n_wires,
-                            has_params=True,
-                            trainable=True,
-                        )
-                    )
-                    self.cu3_layers.append(
-                        tq.Op2QAllLayer(
-                            op=tq.CU3,
-                            n_wires=self.n_wires,
-                            has_params=True,
-                            trainable=True,
-                            circular=True,
-                        )
-                    )
+# def QuantumTrain(model, n_qubit, nw_list_normal, q_depth, device, network_config):
+#     class LewHybridNN(nn.Module):
+#         class QLayer(nn.Module):
+#             def __init__(self, n_blocks):
+#                 super().__init__()
+#                 self.n_wires = int(np.ceil(np.log2(len(nw_list_normal)))),
+#                 self.n_wires = self.n_wires[0]
+#                 self.n_blocks = n_blocks
+#                 self.u3_layers = tq.QuantumModuleList()
+#                 self.cu3_layers = tq.QuantumModuleList()
+#                 # self.measure = tq.MeasureAll(tq.PauliZ)
+#                 for _ in range(self.n_blocks):
+#                     self.u3_layers.append(
+#                         tq.Op1QAllLayer(
+#                             op=tq.U3,
+#                             n_wires=self.n_wires,
+#                             has_params=True,
+#                             trainable=True,
+#                         )
+#                     )
+#                     self.cu3_layers.append(
+#                         tq.Op2QAllLayer(
+#                             op=tq.CU3,
+#                             n_wires=self.n_wires,
+#                             has_params=True,
+#                             trainable=True,
+#                             circular=True,
+#                         )
+#                     )
                     
-            def forward(self):
-                qdev = tq.QuantumDevice(
-                    n_wires=self.n_wires, bsz=1, device=next(self.parameters()).device
-                )
-                easy_scale_coeff = 2**(n_qubit-1)
-                gamma = 0.1
-                beta  = 0.8
-                alpha = 0.3
-                for k in range(self.n_blocks):
-                    self.u3_layers[k](qdev)
-                    self.cu3_layers[k](qdev)
+#             def forward(self):
+#                 qdev = tq.QuantumDevice(
+#                     n_wires=self.n_wires, bsz=1, device=next(self.parameters()).device
+#                 )
+#                 easy_scale_coeff = 2**(n_qubit-1)
+#                 gamma = 0.1
+#                 beta  = 0.8
+#                 alpha = 0.3
+#                 for k in range(self.n_blocks):
+#                     self.u3_layers[k](qdev)
+#                     self.cu3_layers[k](qdev)
                     
-                state_mag = qdev.get_states_1d().abs()[0] 
-                state_mag = state_mag[:len(nw_list_normal)]
-                x = torch.abs(state_mag) ** 2
-                # x = torch.log(x)
-                x = x.reshape(len(nw_list_normal),1)
-                x = (beta*torch.tanh(gamma*easy_scale_coeff*x))**(alpha) 
-                x = x - torch.mean(x)
-                x.to(device)
-                return x
+#                 state_mag = qdev.get_states_1d().abs()[0] 
+#                 state_mag = state_mag[:len(nw_list_normal)]
+#                 x = torch.abs(state_mag) ** 2
+#                 # x = torch.log(x)
+#                 x = x.reshape(len(nw_list_normal),1)
+#                 x = (beta*torch.tanh(gamma*easy_scale_coeff*x))**(alpha) 
+#                 x = x - torch.mean(x)
+#                 x.to(device)
+#                 return x
             
             
-        class MappingModel(nn.Module):
-            def __init__(self, input_size, hidden_sizes, output_size):
-                super().__init__()
-                # Initialize layers: an input layer, multiple hidden layers, and an output layer
-                self.input_layer = nn.Linear(input_size, hidden_sizes[0])
-                self.hidden_layers = nn.ModuleList([nn.Linear(hidden_sizes[i], hidden_sizes[i+1]) for i in range(len(hidden_sizes)-1)])
-                self.output_layer = nn.Linear(hidden_sizes[-1], output_size)
+#         class MappingModel(nn.Module):
+#             def __init__(self, input_size, hidden_sizes, output_size):
+#                 super().__init__()
+#                 # Initialize layers: an input layer, multiple hidden layers, and an output layer
+#                 self.input_layer = nn.Linear(input_size, hidden_sizes[0])
+#                 self.hidden_layers = nn.ModuleList([nn.Linear(hidden_sizes[i], hidden_sizes[i+1]) for i in range(len(hidden_sizes)-1)])
+#                 self.output_layer = nn.Linear(hidden_sizes[-1], output_size)
                 
-            def forward(self, X):
-                # Ensure the input tensor is the same type as the weights
-                X = X.type_as(self.input_layer.weight)
+#             def forward(self, X):
+#                 # Ensure the input tensor is the same type as the weights
+#                 X = X.type_as(self.input_layer.weight)
 
-                # Input layer with ReLU activation
-                X = self.input_layer(X)
+#                 # Input layer with ReLU activation
+#                 X = self.input_layer(X)
 
-                # Hidden layers with ReLU activation
-                for hidden in self.hidden_layers:
-                    X = hidden(X)
+#                 # Hidden layers with ReLU activation
+#                 for hidden in self.hidden_layers:
+#                     X = hidden(X)
 
-                # Output layer with linear activation
-                output = self.output_layer(X)
-                # output = F.tanh(output)  # It's often better to use ReLU or similar; tanh is used here as it was in the original model.
-                return output
+#                 # Output layer with linear activation
+#                 output = self.output_layer(X)
+#                 # output = F.tanh(output)  # It's often better to use ReLU or similar; tanh is used here as it was in the original model.
+#                 return output
 
-        def __init__(self):
-            """
-            """
+#         def __init__(self):
+#             """
+#             """
+#             super().__init__()
+#             self.MappingNetwork = self.MappingModel(n_qubit+1, [4, 20, 4], 1).to(device)  
+#             self.QuantumNN = self.QLayer(q_depth).to(device)   #arch={"n_blocks": q_depth})
+#             # self.reconstruct_model = model.to(device)
+        
+#         def forward(self, x):
+#             """
+#             """
+#             device = x.device
+
+#             probs_ = self.QuantumNN()
+#             probs_ = probs_[:len(nw_list_normal)]
+            
+#             # Generate qubit states using PyTorch
+#             qubit_states_torch = generate_qubit_states_torch(n_qubit)[:len(nw_list_normal)]
+#             qubit_states_torch = qubit_states_torch.to(device)
+#             # Combine qubit states with probability values using PyTorch
+            
+#             combined_data_torch = torch.cat((qubit_states_torch, probs_), dim=1)
+#             combined_data_torch = combined_data_torch.reshape(len(nw_list_normal), 1, n_qubit+1)
+            
+#             prob_val_post_processed = self.MappingNetwork(combined_data_torch)
+#             prob_val_post_processed = prob_val_post_processed - prob_val_post_processed.mean()
+            
+#             state_dict = probs_to_weights(prob_val_post_processed, model)
+            
+            
+#             dtype = torch.float32
+#             for layer in network_config:
+#                 x = apply_layer(x, layer, state_dict, device, dtype)
+            
+#             return x 
+        
+#     return LewHybridNN
+
+
+
+class LewHybridNN(nn.Module):
+    def __init__(self, model, MappingNetwork, QuantumNN, nw_list_normal, network_config, n_qubit ,device):
+        """
+        """
+        super().__init__()
+        self.model = model
+        self.MappingNetwork = MappingNetwork
+        self.QuantumNN = QuantumNN
+        self.nw_list_normal = nw_list_normal
+        self.network_config = network_config
+        self.n_qubit = n_qubit
+        self.device = device
+
+    def forward(self, x):
+        """
+        """
+        device = x.device
+        prob_len = len(self.nw_list_normal)
+        probs_ = self.QuantumNN()
+        probs_ = probs_[:prob_len]
+
+        n_qubit = self.n_qubit
+    
+        # Generate qubit states using PyTorch
+        qubit_states_torch = generate_qubit_states_torch(n_qubit)[:prob_len]
+        qubit_states_torch = qubit_states_torch.to(device)
+        # Combine qubit states with probability values using PyTorch
+    
+        combined_data_torch = torch.cat((qubit_states_torch, probs_), dim=1)
+        combined_data_torch = combined_data_torch.reshape(prob_len, 1, n_qubit+1)
+    
+        prob_val_post_processed = self.MappingNetwork(combined_data_torch)
+        prob_val_post_processed = prob_val_post_processed - prob_val_post_processed.mean()
+    
+        state_dict = probs_to_weights(prob_val_post_processed, self.model)
+    
+    
+        dtype = torch.float32
+        for layer in self.network_config:
+            x = apply_layer(x, layer, state_dict, device, dtype)
+    
+        return x 
+    
+    def QuantumTrain(model, n_qubit, nw_list_normal, q_depth, device, network_config):
+        MappingNetwork = LewHybridNN.MappingModel(n_qubit+1, [4, 20, 4], 1).to(device)  
+        QuantumNN = LewHybridNN.QLayer(q_depth, nw_list_normal).to(device)   #arch={"n_blocks": q_depth})
+  
+        return LewHybridNN(model, MappingNetwork, QuantumNN, nw_list_normal, network_config, n_qubit, device)
+
+    class QLayer(nn.Module):
+        def __init__(self, n_blocks, nw_list_normal=None):
             super().__init__()
-            self.MappingNetwork = self.MappingModel(n_qubit+1, [4, 20, 4], 1).to(device)  
-            self.QuantumNN = self.QLayer(q_depth).to(device)   #arch={"n_blocks": q_depth})
-            # self.reconstruct_model = model.to(device)
-        
-        def forward(self, x):
-            """
-            """
-            device = x.device
 
-            probs_ = self.QuantumNN()
-            probs_ = probs_[:len(nw_list_normal)]
+            self.nw_list_normal = nw_list_normal
+            self.n_wires = int(np.ceil(np.log2(len(nw_list_normal)))),
+            self.n_wires = self.n_wires[0]
+            self.n_blocks = n_blocks
+            self.u3_layers = tq.QuantumModuleList()
+            self.cu3_layers = tq.QuantumModuleList()
+            # self.measure = tq.MeasureAll(tq.PauliZ)
+            for _ in range(self.n_blocks):
+                self.u3_layers.append(
+                    tq.Op1QAllLayer(
+                        op=tq.U3,
+                        n_wires=self.n_wires,
+                        has_params=True,
+                        trainable=True,
+                    )
+                )
+                self.cu3_layers.append(
+                    tq.Op2QAllLayer(
+                        op=tq.CU3,
+                        n_wires=self.n_wires,
+                        has_params=True,
+                        trainable=True,
+                        circular=True,
+                    )
+                )
             
-            # Generate qubit states using PyTorch
-            qubit_states_torch = generate_qubit_states_torch(n_qubit)[:len(nw_list_normal)]
-            qubit_states_torch = qubit_states_torch.to(device)
-            # Combine qubit states with probability values using PyTorch
+        def forward(self):
+            qdev = tq.QuantumDevice(
+                n_wires=self.n_wires, bsz=1, device=next(self.parameters()).device
+            )
+            easy_scale_coeff = 2**(self.n_qubit-1)
+            gamma = 0.1
+            beta  = 0.8
+            alpha = 0.3
+            for k in range(self.n_blocks):
+                self.u3_layers[k](qdev)
+                self.cu3_layers[k](qdev)
             
-            combined_data_torch = torch.cat((qubit_states_torch, probs_), dim=1)
-            combined_data_torch = combined_data_torch.reshape(len(nw_list_normal), 1, n_qubit+1)
-            
-            prob_val_post_processed = self.MappingNetwork(combined_data_torch)
-            prob_val_post_processed = prob_val_post_processed - prob_val_post_processed.mean()
-            
-            state_dict = probs_to_weights(prob_val_post_processed, model)
-            
-            
-            dtype = torch.float32
-            for layer in network_config:
-                x = apply_layer(x, layer, state_dict, device, dtype)
-            
-            return x 
+            state_mag = qdev.get_states_1d().abs()[0] 
+            state_mag = state_mag[:len(self.nw_list_normal)]
+            x = torch.abs(state_mag) ** 2
+            # x = torch.log(x)
+            x = x.reshape(len(self.nw_list_normal),1)
+            x = (beta*torch.tanh(gamma*easy_scale_coeff*x))**(alpha) 
+            x = x - torch.mean(x)
+            x.to(self.device)
+            return x
+    
+    
+    class MappingModel(nn.Module):
+        def __init__(self, input_size, hidden_sizes, output_size):
+            super().__init__()
+            # Initialize layers: an input layer, multiple hidden layers, and an output layer
+            self.input_layer = nn.Linear(input_size, hidden_sizes[0])
+            self.hidden_layers = nn.ModuleList([nn.Linear(hidden_sizes[i], hidden_sizes[i+1]) for i in range(len(hidden_sizes)-1)])
+            self.output_layer = nn.Linear(hidden_sizes[-1], output_size)
         
-    return LewHybridNN
+        def forward(self, X):
+            # Ensure the input tensor is the same type as the weights
+            X = X.type_as(self.input_layer.weight)
 
+            # Input layer with ReLU activation
+            X = self.input_layer(X)
+
+            # Hidden layers with ReLU activation
+            for hidden in self.hidden_layers:
+                X = hidden(X)
+
+            # Output layer with linear activation
+            output = self.output_layer(X)
+            # output = F.tanh(output)  # It's often better to use ReLU or similar; tanh is used here as it was in the original model.
+            return output
 
 """
 network_config = []
